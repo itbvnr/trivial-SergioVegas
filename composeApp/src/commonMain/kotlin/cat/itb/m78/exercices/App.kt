f68ebc8a85f2d1336801098b9bae414cae5f092b
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-02-10 13:03:10 +0100

    Inicio

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -6,12 +6,13 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import cat.itb.m78.exercices.Trivial_App.MenuScreen
 import cat.itb.m78.exercices.theme.AppTheme
 import org.jetbrains.compose.reload.DevelopmentEntryPoint
 
 @Composable
 internal fun App() = AppTheme {
     Box(Modifier.fillMaxSize()){
-        Text("Your app goes here!", Modifier.align(Alignment.Center))
+        MenuScreen();
     }
 }
---
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Trivial/Menu.kt
@@ -0,0 +1,76 @@
+package cat.itb.m78.exercices.Trivial_App
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.ColumnScope.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import cat.itb.m78.exercices.theme.AppTheme
+import kotlinx.coroutines.launch
+import m78exercices.composeapp.generated.resources.Res
+import org.jetbrains.compose.resources.painterResource
+import m78exercices.composeapp.generated.resources.Dragonite
+import m78exercices.composeapp.generated.resources.Pokemon
+
+
+@Composable
+internal fun MenuScreen() = AppTheme {
+    Box(Modifier.fillMaxSize().background(Color.Black)) {
+
+        Image(
+            painter = painterResource(Res.drawable.Dragonite),
+            modifier = Modifier.fillMaxSize(),
+            contentDescription = null
+        )
+        Column(
+            Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Image(
+                painter = painterResource(Res.drawable.Pokemon),
+                modifier = Modifier.width(200.dp).height(200.dp),
+                contentDescription = null
+            )
+
+            //boto 1
+            Button(onClick = {
+
+            }){
+                Text("New Game")
+            }
+
+            //boton 2
+            Button(onClick = {
+
+            })
+            { Text("Settings")
+            }
+        }
+    }
+}
+
+@Composable
+internal fun GameScreen() = AppTheme {
+    Box(Modifier.fillMaxSize().background(Color.Black)) {
+
+        Column(
+            Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+           Text(
+               text =
+           )
+        }
+    }
+}
+
